7f53941e746a6a6813db7cb3bf7dc32507da770d
Andrej Mazic <andrejmazicdev@gmail.com>
2022-08-17 21:08:58 +0200

    Test

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
 export function checkIfUserExist(id) {
     const url = `https://fantasy.premierleague.com/api/entry/${id}/`;
     return fetch(url, {
         headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' }
     })
-};
-
+}
